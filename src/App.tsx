c27081b11c2479559098646857d9c3ccbbd86a1b
Artuur Alexandrov <naturahxq@gmail.com>
2024-02-01 23:07:32 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, useState } from "react";
+import AddPizzaForm from "./components/AddPizzaForm";
+import DisplayPizzas from "./components/DisplayPizzas";
+import Pizza from "./models/Pizza";
+import "./App.css";
 
-function App() {
+const App: FC = () => {
+  const [pizzasList, setPizzasList] = useState<Pizza[]>([]);
+
+  const addPizza = (newPizza: Pizza) => {
+    setPizzasList([...pizzasList, newPizza]);
+  };
+
+  const updatePizza = (newPizza: Pizza) => {
+    setPizzasList(
+      pizzasList.map((pizza) => (pizza.id === newPizza.id ? newPizza : pizza))
+    );
+  };
+
+  const deletePizza = (id: number) => {
+    const newPizzasList = pizzasList.filter((pizza) => pizza.id !== id);
+    setPizzasList(newPizzasList);
+  };
+
+  console.log("pizzasList >>>>> ", pizzasList);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrap">
+        <span className="heading">Пиццерия</span>
+        <AddPizzaForm addPizza={addPizza} />
+
+        <DisplayPizzas
+          pizzasList={pizzasList}
+          deletePizza={deletePizza}
+          updatePizza={updatePizza}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
